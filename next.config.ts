d899d8a0398d5e21d42b76ce54b1423d651161ef
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2025-10-08 13:09:40 +0330

    .

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // اضافه کردن این بخش برای ignore کردن خطاها
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
